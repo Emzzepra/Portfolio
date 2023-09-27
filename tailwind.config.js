7d55765fe6077dd745dd8801431577875feb9943
Emmanuel Ajala <127834217+Emzzepra@users.noreply.github.com>
2023-09-27 23:58:04 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        home : '#222831',
+        primary : '#ccd5f3',
+        hover : '#18b9cfe6',
+        declaration : '#b2b9cf',
+        span : ' #1de4ff',
+        project : ' #263243',
+        footer : '#8f94a4',
+        top : '#263243',
+      }
+    },
+  },
+  plugins: [],
+}
+
+
